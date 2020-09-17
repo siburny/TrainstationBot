7ed616a1c963d4bd8f98ca98cdbadd2c1f1279f2
Maxim Rubis <siburny@gmail.com>
2020-09-16 21:01:48 -0400

    Initial commit

---
+++ TrainStationBot/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace TrainStationBot
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartStopButton = new System.Windows.Forms.Button();
+            this.OutputListBox = new System.Windows.Forms.ListBox();
+            this.PanelScores = new System.Windows.Forms.Panel();
+            this.TextboxScore8 = new System.Windows.Forms.TextBox();
+            this.TextboxScore7 = new System.Windows.Forms.TextBox();
+            this.TextboxScore6 = new System.Windows.Forms.TextBox();
+            this.TextboxScore5 = new System.Windows.Forms.TextBox();
+            this.TextboxScore4 = new System.Windows.Forms.TextBox();
+            this.TextboxScore3 = new System.Windows.Forms.TextBox();
+            this.TextboxScore2 = new System.Windows.Forms.TextBox();
+            this.TextboxScore1 = new System.Windows.Forms.TextBox();
+            this.PictureBoxOutput = new System.Windows.Forms.PictureBox();
+            this.PanelScores.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StartStopButton
+            // 
+            this.StartStopButton.Font = new System.Drawing.Font("Calibri", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartStopButton.Location = new System.Drawing.Point(12, 12);
+            this.StartStopButton.Name = "StartStopButton";
+            this.StartStopButton.Size = new System.Drawing.Size(169, 66);
+            this.StartStopButton.TabIndex = 0;
+            this.StartStopButton.Text = "Start";
+            this.StartStopButton.UseVisualStyleBackColor = true;
+            this.StartStopButton.Click += new System.EventHandler(this.StartStopButton_Click);
+            // 
+            // OutputListBox
+            // 
+            this.OutputListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.OutputListBox.FormattingEnabled = true;
+            this.OutputListBox.ItemHeight = 22;
+            this.OutputListBox.Location = new System.Drawing.Point(12, 84);
+            this.OutputListBox.Name = "OutputListBox";
+            this.OutputListBox.Size = new System.Drawing.Size(941, 1170);
+            this.OutputListBox.TabIndex = 3;
+            // 
+            // PanelScores
+            // 
+            this.PanelScores.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.PanelScores.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PanelScores.Controls.Add(this.TextboxScore8);
+            this.PanelScores.Controls.Add(this.TextboxScore7);
+            this.PanelScores.Controls.Add(this.TextboxScore6);
+            this.PanelScores.Controls.Add(this.TextboxScore5);
+            this.PanelScores.Controls.Add(this.TextboxScore4);
+            this.PanelScores.Controls.Add(this.TextboxScore3);
+            this.PanelScores.Controls.Add(this.TextboxScore2);
+            this.PanelScores.Controls.Add(this.TextboxScore1);
+            this.PanelScores.Location = new System.Drawing.Point(959, 84);
+            this.PanelScores.Name = "PanelScores";
+            this.PanelScores.Size = new System.Drawing.Size(411, 341);
+            this.PanelScores.TabIndex = 4;
+            // 
+            // TextboxScore8
+            // 
+            this.TextboxScore8.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextboxScore8.Location = new System.Drawing.Point(3, 297);
+            this.TextboxScore8.Name = "TextboxScore8";
+            this.TextboxScore8.ReadOnly = true;
+            this.TextboxScore8.Size = new System.Drawing.Size(403, 36);
+            this.TextboxScore8.TabIndex = 7;
+            // 
+            // TextboxScore7
+            // 
+            this.TextboxScore7.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextboxScore7.Location = new System.Drawing.Point(3, 255);
+            this.TextboxScore7.Name = "TextboxScore7";
+            this.TextboxScore7.ReadOnly = true;
+            this.TextboxScore7.Size = new System.Drawing.Size(403, 36);
+            this.TextboxScore7.TabIndex = 6;
+            // 
+            // TextboxScore6
+            // 
+            this.TextboxScore6.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextboxScore6.Location = new System.Drawing.Point(3, 213);
+            this.TextboxScore6.Name = "TextboxScore6";
+            this.TextboxScore6.ReadOnly = true;
+            this.TextboxScore6.Size = new System.Drawing.Size(403, 36);
+            this.TextboxScore6.TabIndex = 5;
+            // 
+            // TextboxScore5
+            // 
+            this.TextboxScore5.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextboxScore5.Location = new System.Drawing.Point(3, 171);
+            this.TextboxScore5.Name = "TextboxScore5";
+            this.TextboxScore5.ReadOnly = true;
+            this.TextboxScore5.Size = new System.Drawing.Size(403, 36);
+            this.TextboxScore5.TabIndex = 4;
+            // 
+            // TextboxScore4
+            // 
+            this.TextboxScore4.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextboxScore4.Location = new System.Drawing.Point(3, 129);
+            this.TextboxScore4.Name = "TextboxScore4";
+            this.TextboxScore4.ReadOnly = true;
+            this.TextboxScore4.Size = new System.Drawing.Size(403, 36);
+            this.TextboxScore4.TabIndex = 3;
+            // 
+            // TextboxScore3
+            // 
+            this.TextboxScore3.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextboxScore3.Location = new System.Drawing.Point(3, 87);
+            this.TextboxScore3.Name = "TextboxScore3";
+            this.TextboxScore3.ReadOnly = true;
+            this.TextboxScore3.Size = new System.Drawing.Size(403, 36);
+            this.TextboxScore3.TabIndex = 2;
+            // 
+            // TextboxScore2
+            // 
+            this.TextboxScore2.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextboxScore2.Location = new System.Drawing.Point(3, 45);
+            this.TextboxScore2.Name = "TextboxScore2";
+            this.TextboxScore2.ReadOnly = true;
+            this.TextboxScore2.Size = new System.Drawing.Size(403, 36);
+            this.TextboxScore2.TabIndex = 1;
+            // 
+            // TextboxScore1
+            // 
+            this.TextboxScore1.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextboxScore1.Location = new System.Drawing.Point(3, 3);
+            this.TextboxScore1.Name = "TextboxScore1";
+            this.TextboxScore1.ReadOnly = true;
+            this.TextboxScore1.Size = new System.Drawing.Size(403, 36);
+            this.TextboxScore1.TabIndex = 0;
+            // 
+            // PictureBoxOutput
+            // 
+            this.PictureBoxOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PictureBoxOutput.Location = new System.Drawing.Point(12, 1263);
+            this.PictureBoxOutput.Name = "PictureBoxOutput";
+            this.PictureBoxOutput.Size = new System.Drawing.Size(1358, 427);
+            this.PictureBoxOutput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBoxOutput.TabIndex = 5;
+            this.PictureBoxOutput.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(1382, 1702);
+            this.Controls.Add(this.PictureBoxOutput);
+            this.Controls.Add(this.PanelScores);
+            this.Controls.Add(this.OutputListBox);
+            this.Controls.Add(this.StartStopButton);
+            this.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.PanelScores.ResumeLayout(false);
+            this.PanelScores.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxOutput)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartStopButton;
+        private System.Windows.Forms.ListBox OutputListBox;
+        private System.Windows.Forms.Panel PanelScores;
+        private System.Windows.Forms.TextBox TextboxScore1;
+        private System.Windows.Forms.TextBox TextboxScore7;
+        private System.Windows.Forms.TextBox TextboxScore6;
+        private System.Windows.Forms.TextBox TextboxScore5;
+        private System.Windows.Forms.TextBox TextboxScore4;
+        private System.Windows.Forms.TextBox TextboxScore3;
+        private System.Windows.Forms.TextBox TextboxScore2;
+        private System.Windows.Forms.PictureBox PictureBoxOutput;
+        private System.Windows.Forms.TextBox TextboxScore8;
+    }
+}
+
---
+++ TrainStationBot/Form1.cs
@@ -0,0 +1,379 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+using TrainStationBot.Properties;
+using Color = System.Drawing.Color;
+using SystemColors = System.Drawing.SystemColors;
+
+namespace TrainStationBot
+{
+    public partial class MainForm : Form
+    {
+        private IntPtr GameWindowHandle;
+        private ScreenUtilities.RECT GameWindowPosition;
+
+        private bool IsActive = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadImages();
+        }
+
+        private readonly List<Mat> ads = new List<Mat>();
+        Mat arrow, box, whistle, levelup, bonus, balloon, dailyreward;
+
+        private void LoadImages()
+        {
+            ads.Add(new Mat(@"c:\Projects\C#\TrainStationBot\TrainStationBot\images\ads\ad1.png").CvtColor(ColorConversionCodes.BGR2GRAY));
+            ads.Add(new Mat(@"c:\Projects\C#\TrainStationBot\TrainStationBot\images\ads\ad2.png").CvtColor(ColorConversionCodes.BGR2GRAY));
+            ads.Add(new Mat(@"c:\Projects\C#\TrainStationBot\TrainStationBot\images\ads\ad3.png").CvtColor(ColorConversionCodes.BGR2GRAY));
+            ads.Add(new Mat(@"c:\Projects\C#\TrainStationBot\TrainStationBot\images\ads\ad4.png").CvtColor(ColorConversionCodes.BGR2GRAY));
+            ads.Add(new Mat(@"c:\Projects\C#\TrainStationBot\TrainStationBot\images\ads\ad5.png").CvtColor(ColorConversionCodes.BGR2GRAY));
+
+            arrow = new Mat(@"c:\Projects\C#\TrainStationBot\TrainStationBot\images\arrow.png").CvtColor(ColorConversionCodes.BGR2GRAY);
+            box = new Mat(@"c:\Projects\C#\TrainStationBot\TrainStationBot\images\box.png").CvtColor(ColorConversionCodes.BGR2GRAY);
+            whistle = new Mat(@"c:\Projects\C#\TrainStationBot\TrainStationBot\images\whistle.png").CvtColor(ColorConversionCodes.BGR2GRAY);
+            levelup = new Mat(@"c:\Projects\C#\TrainStationBot\TrainStationBot\images\levelup.png").CvtColor(ColorConversionCodes.BGR2GRAY);
+            bonus = new Mat(@"c:\Projects\C#\TrainStationBot\TrainStationBot\images\bonus.png").CvtColor(ColorConversionCodes.BGR2GRAY);
+            balloon = new Mat(@"c:\Projects\C#\TrainStationBot\TrainStationBot\images\balloon.png").CvtColor(ColorConversionCodes.BGR2GRAY);
+            dailyreward = new Mat(@"c:\Projects\C#\TrainStationBot\TrainStationBot\images\dailyreward.png").CvtColor(ColorConversionCodes.BGR2GRAY);
+        }
+
+        private void StartStopButton_Click(object sender, EventArgs e)
+        {
+            if (IsActive)
+            {
+                IsActive = false;
+                StartStopButton.Text = "START";
+            }
+            else
+            {
+                IsActive = true;
+                StartStopButton.Text = "STOP";
+
+                GameWindowHandle = ScreenUtilities.GetGameWindow();
+                if (GameWindowHandle == default)
+                {
+                    MessageBox.Show("Can't find browser window");
+                    Application.Exit();
+                }
+
+                ScreenUtilities.GetWindowRect(GameWindowHandle, ref GameWindowPosition);
+
+                Thread child = new Thread(new ThreadStart(this.TimerLoop));
+                child.Start();
+            }
+        }
+
+        private Mat screen_rgb;
+        private Mat screen_gray;
+        private void TimerLoop()
+        {
+            while (true)
+            {
+                AddOutput("Analyzing screenshot");
+
+                using (var image = ScreenUtilities.CaptureWindow(GameWindowHandle))
+                {
+                    image.Save(@"c:\Projects\C#\TrainStationBot\TrainStationBot\images\output\screen-" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".jpg");
+
+                    screen_rgb = image.ToMat();
+                    screen_gray = screen_rgb.CvtColor(ColorConversionCodes.BGRA2GRAY);
+
+                    if (!IsActive) return;
+                    CheckForBalloon();
+
+                    if (!IsActive) return;
+                    CheckForAds();
+
+                    if (!IsActive) return;
+                    CheckForLevelup();
+
+                    if (!IsActive) return;
+                    CheckForDailyReward();
+
+                    if (!IsActive) return;
+                    CheckForBox();
+
+                    if (!IsActive) return;
+                    CheckForBonus();
+
+                    if (!IsActive) return;
+                    CheckForTrains();
+
+                    if (!IsActive) return;
+                    CheckForWhistle();
+
+                    if (!IsActive) return;
+                    Invoke(new Action(() => { PictureBoxOutput.Image = screen_rgb.ToBitmap(); }));
+
+                    screen_rgb.Dispose();
+                    screen_gray.Dispose();
+                }
+
+                Application.DoEvents();
+                if (!IsActive) return;
+            }
+        }
+
+        private void CheckForAds()
+        {
+            foreach (var ad in ads)
+            {
+                Detect("ad", ad, out double maxValue, out Point maxLocation);
+
+                if (maxValue > 0.9)
+                {
+                    AddOutput("Found AD at " + maxLocation.X + "x" + maxLocation.Y + ": clicked");
+                    MouseOperations.MouseClick(maxLocation.X + GameWindowPosition.left + ad.Width - 10, maxLocation.Y + GameWindowPosition.top + 10);
+
+                    UpdateScore(TextboxScore1, "AD: " + maxValue.ToString(), Color.LightGreen);
+                }
+                else
+                {
+                    UpdateScore(TextboxScore1, "AD: " + maxValue.ToString(), SystemColors.Control);
+                }
+            }
+        }
+
+        private void CheckForBox()
+        {
+            Detect("box", box, out double maxValue, out Point maxLocation);
+
+            if (maxValue > 0.9)
+            {
+                AddOutput("Found BOX at " + maxLocation.X + "x" + maxLocation.Y + ": clicked it");
+                MouseOperations.MouseClick(maxLocation.X + GameWindowPosition.left + 20, 750 + GameWindowPosition.top);
+
+                WaitNSeconds(100);
+
+                MouseOperations.MouseClick(840 + GameWindowPosition.left, 812 + GameWindowPosition.top);
+
+                UpdateScore(TextboxScore2, "BOX: " + maxValue.ToString(), Color.LightGreen);
+            }
+            else
+            {
+                UpdateScore(TextboxScore2, "BOX: " + maxValue.ToString(), SystemColors.Control);
+            }
+        }
+
+        private void CheckForBalloon()
+        {
+            Detect("balloon", balloon, out double maxValue, out Point maxLocation);
+
+            if (maxValue > 0.9)
+            {
+                AddOutput("Found BALLOON: " + maxLocation.X + "x" + maxLocation.Y);
+                MouseOperations.MouseClick(maxLocation.X + GameWindowPosition.left + 20, maxLocation.Y + GameWindowPosition.top);
+
+                UpdateScore(TextboxScore3, "BALLOON: " + maxValue.ToString(), Color.LightGreen);
+            }
+            else
+            {
+                UpdateScore(TextboxScore3, "BALLOON: " + maxValue.ToString(), SystemColors.Control);
+            }
+        }
+
+        private void CheckForLevelup()
+        {
+            Detect("LEVEL UP", levelup, out double maxValue, out Point maxLocation);
+
+            if (maxValue > 0.9)
+            {
+                AddOutput("Found LEVEL UP: " + maxLocation.X + "x" + maxLocation.Y);
+                MouseOperations.MouseClick(maxLocation.X + GameWindowPosition.left + 20, 750 + GameWindowPosition.top);
+                WaitNSeconds(100);
+
+                MouseOperations.MouseClick(840 + GameWindowPosition.left + 20, 884 + GameWindowPosition.top);
+
+                UpdateScore(TextboxScore4, "LEVEL UP: " + maxValue.ToString(), Color.LightGreen);
+            }
+            else
+            {
+                UpdateScore(TextboxScore4, "LEVEL UP: " + maxValue.ToString(), SystemColors.Control);
+            }
+        }
+
+        private void CheckForDailyReward()
+        {
+            Detect("reward", dailyreward, out double maxValue, out Point maxLocation);
+
+            if (maxValue > 0.9)
+            {
+                AddOutput("Found DAILY REWARD: " + maxLocation.X + "x" + maxLocation.Y);
+                MouseOperations.MouseClick(840 + GameWindowPosition.left + 20, 779 + GameWindowPosition.top);
+
+                UpdateScore(TextboxScore8, "DAILY REWARD: " + maxValue.ToString(), Color.LightGreen);
+            }
+            else
+            {
+                UpdateScore(TextboxScore8, "DAILY REWARD: " + maxValue.ToString(), SystemColors.Control);
+            }
+        }
+
+        private void CheckForBonus()
+        {
+            Detect("bonus", bonus, out double maxValue, out Point maxLocation);
+
+            if (maxValue > 0.9)
+            {
+                AddOutput("Found BONUS: " + maxLocation.X + "x" + maxLocation.Y);
+                MouseOperations.MouseClick(maxLocation.X + GameWindowPosition.left + 20, maxLocation.Y + 20 + GameWindowPosition.top);
+
+                UpdateScore(TextboxScore5, "BONUS: " + maxValue.ToString(), Color.LightGreen);
+            }
+            else
+            {
+                UpdateScore(TextboxScore5, "BONUS: " + maxValue.ToString(), SystemColors.Control);
+            }
+        }
+
+        private void CheckForTrains()
+        {
+            Detect("train", arrow, out double maxValue, out Point maxLocation);
+
+            if (maxValue > 0.9)
+            {
+                AddOutput("Found TRAIN: " + maxLocation.X + "x" + maxLocation.Y);
+
+                MouseOperations.MouseClick(maxLocation.X + GameWindowPosition.left + 20, 839 + GameWindowPosition.top);
+
+                WaitNSeconds(250);
+
+                MouseOperations.MouseClick(maxLocation.X + GameWindowPosition.left + 20, 839 + GameWindowPosition.top);
+
+                WaitNSeconds(250);
+
+                MouseOperations.MouseClick(520 + GameWindowPosition.left, 906 + GameWindowPosition.top);
+
+                WaitNSeconds(250);
+
+                MouseOperations.MouseClick(920 + GameWindowPosition.left, 730 + GameWindowPosition.top);
+
+                UpdateScore(TextboxScore6, "TRAIN: " + maxValue.ToString(), Color.LightGreen);
+            }
+            else
+            {
+                UpdateScore(TextboxScore6, "TRAIN: " + maxValue.ToString(), SystemColors.Control);
+            }
+        }
+
+        private void CheckForWhistle()
+        {
+            Detect("whistle", whistle, out double maxValue, out Point maxLocation);
+
+            if (maxValue > 0.95)
+            {
+                AddOutput("Found WHISTLE: " + maxLocation.X + "x" + maxLocation.Y);
+                MouseOperations.MouseClick(maxLocation.X + GameWindowPosition.left + 20, maxLocation.Y + 20 + GameWindowPosition.top);
+
+                UpdateScore(TextboxScore7, "WHISTLE: " + maxValue.ToString(), Color.LightGreen);
+            }
+            else
+            {
+                UpdateScore(TextboxScore7, "WHISTLE: " + maxValue.ToString(), SystemColors.Control);
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            RestoreWindowPosition();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            IsActive = false;
+            SaveWindowPosition();
+        }
+
+        private void RestoreWindowPosition()
+        {
+            if (Settings.Default.HasSetDefaults)
+            {
+                this.WindowState = Settings.Default.WindowState;
+                this.Location = Settings.Default.Location;
+                this.Size = Settings.Default.Size;
+            }
+        }
+
+        private void SaveWindowPosition()
+        {
+            Settings.Default.WindowState = this.WindowState;
+
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                Settings.Default.Location = this.Location;
+                Settings.Default.Size = this.Size;
+            }
+            else
+            {
+                Settings.Default.Location = this.RestoreBounds.Location;
+                Settings.Default.Size = this.RestoreBounds.Size;
+            }
+
+            Settings.Default.HasSetDefaults = true;
+
+            Settings.Default.Save();
+        }
+
+        private void AddOutput(string text)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(() => AddOutput(text)));
+            }
+            else
+            {
+                OutputListBox.Items.Add(DateTime.Now.ToString() + ": " + text);
+                OutputListBox.TopIndex = OutputListBox.Items.Count - 1;
+            }
+        }
+
+        private void UpdateScore(Control control, String text, System.Drawing.Color color)
+        {
+            if (control.InvokeRequired)
+            {
+                this.Invoke(new Action(() => UpdateScore(control, text, color)));
+            }
+            else
+            {
+                control.Text = text;
+                control.BackColor = color;
+            }
+        }
+
+        private void WaitNSeconds(int ms)
+        {
+            System.Threading.Thread.Sleep(ms);
+            return;
+        }
+
+        private void Detect(string name, Mat template, out double maxValue, out Point maxLocation)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            using (Mat result = screen_gray.MatchTemplate(template, TemplateMatchModes.CCoeffNormed))
+            {
+                watch.Stop();
+                Console.WriteLine("Time: " + 1.0 * watch.ElapsedMilliseconds / 1000);
+
+                Cv2.MinMaxLoc(result, out _, out maxValue, out _, out maxLocation);
+                Console.WriteLine("maxVal: " + maxValue + " maxLoc: " + maxLocation);
+
+                if (maxValue > 0.9)
+                {
+                    screen_rgb.Rectangle(new Rect(maxLocation.X, maxLocation.Y, template.Width, template.Height), Scalar.LightGreen, 3);
+                    screen_rgb.PutText(name, new Point(maxLocation.X, maxLocation.Y), HersheyFonts.HersheyPlain, 2, Scalar.White, 3);
+                }
+                else if (maxValue > 0.5)
+                {
+                    screen_rgb.Rectangle(new Rect(maxLocation.X, maxLocation.Y, template.Width, template.Height), Scalar.LightCoral, 3);
+                    screen_rgb.PutText(name, new Point(maxLocation.X, maxLocation.Y), HersheyFonts.HersheyPlain, 2, Scalar.White, 3);
+                }
+            }
+        }
+    }
+}
